521a8a1bfb380362c61ca075e138c41d4ed55ddd
Sylvain Bohrer <a3e7d6@gmail.com>
2024-03-12 15:04:28 +0100

    create somes routes _ version 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # user story d'un utilisateur lambda (futur grand-mère ou futur client):
+  authenticate :user do
+    resources :users, only: [:edit, :update] do
+      resources :offers, only: [:index, :new, :create] do
+        resources :bookings, only: [:new, :create]
+      end
+    end
+  end
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,27 +19,26 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :offers, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [:new, :create]
-  end
 
-  resources :bookings, only: [:new, :create]
-    resources :users, only: [:edit, :update] do
-      resources :offers, only: [:new, :create]
-        resources :bookings, only: [:index] do
-          member do
-            patch :accept
-            patch :decline
-          end
-        end
+  # 'visitor' story d'un visiteur:
+  resources :offers, only: [:index, :show]
+
+  # user story d'une grand-mère:
+  namespace :grandma do
+    resources :offers, only: [:new, :create] do
+      resources :bookings, only: [:new, :create]
+    end
+    resources :bookings, only: [:index] do
+      member do
+        patch :accept
+        patch :decline
       end
     end
   end
-
 end
 
 # melissa va rajouter :
 # uikit.html.erb
 # Get ‘uikit’, to: ‘pages’#uikit'
-# localhoast:3000/uikit
+# localhost:3000/uikit
 # Bien créer la méthode uikit dans PagesController
