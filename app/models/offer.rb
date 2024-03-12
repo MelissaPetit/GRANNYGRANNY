2465355953fb6dafdc94679ae2bda82c8cf97309
christophe hubeaux <ch.hubeaux@gmail.com>
2024-03-12 12:02:34 +0100

    model booking & seeds

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,4 +2,6 @@ class Offer < ApplicationRecord
   CATEGORY = ["Sport extrême", "Cusine", "Natation"]
 
   belongs_to :user
+  validates :title, :start_from, :price_per_participant, :photo_url, :duration, :category, :address, :capacity, presence: true
+  validates :description, length: { minimum: 10 }
 end
