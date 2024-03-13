d90692ab0e36ba3fe910600f6fb18bb48cad4fba
Sylvain Bohrer <a3e7d6@gmail.com>
2024-03-13 12:45:21 +0100

    ajout des liens sur les card category de la homepage

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,28 +1,29 @@
 class OffersController < ApplicationController
-
-    def index
-        @offers = Offer.all
-        if params[:query].present?
-            @offers = @offers.where("title ILIKE ?", "%#{params[:query]}%")
-        end
+  def index
+    @offers = Offer.all
+    if params[:query].present?
+      @offers = @offers.where("title ILIKE ?", "%#{params[:query]}%")
+    elsif params[:category].present?
+      @offers = Offer.where(category: params[:category])
     end
+  end
 
-    def show
-        @offer = Offer.find(params[:id])
-        @booking = Booking.new
-        # @booking = Booking.new(booking_params)
-        # @booking.offer = @offer
-        # @booking.user = current_user
-        # if @booking.save
-        #     redirect_to offer_path(@offer)
-        # else
-        #     render :show, status: :unprocessable_entity
-        # end
-    end
-
-    private
-
-    # def booking_params
-    #     params.require(:booking).permit(:date, :participant_number, :comment, :total_price, :status)
+  def show
+    @offer = Offer.find(params[:id])
+    @booking = Booking.new
+    # @booking = Booking.new(booking_params)
+    # @booking.offer = @offer
+    # @booking.user = current_user
+    # if @booking.save
+    #     redirect_to offer_path(@offer)
+    # else
+    #     render :show, status: :unprocessable_entity
     # end
+  end
+
+  # private
+
+  # def booking_params
+  #     params.require(:booking).permit(:date, :participant_number, :comment, :total_price, :status)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   end
   resources :bookings, only: [:index, :destroy]
 
+  resources :offers do
+    get 'category', on: :collection
+  end
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
