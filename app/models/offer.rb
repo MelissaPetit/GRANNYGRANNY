0709ad6a6e9e4eb4abfbb08004681be576190751
christophe hubeaux <ch.hubeaux@gmail.com>
2024-03-13 13:09:08 +0100

    adapt homepage

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,5 @@
 class Offer < ApplicationRecord
-  CATEGORY = ["Sport extrême", "Cuisine", "Natation", "Peinture", "Couture", "Insolite"]
+  CATEGORY = ["Sport", "Cuisine", "Tradition", "Activité manuelle", "Insolite"]
   has_many :bookings, dependent: :destroy
   belongs_to :user
   validates :title, :start_from, :price_per_participant, :photo_url, :duration, :category, :address, :capacity, presence: true
