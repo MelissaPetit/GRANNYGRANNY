f8f6cc61d22fd442eeca6a82b201e88857688748
christophe hubeaux <ch.hubeaux@gmail.com>
2024-03-14 17:44:34 +0100

    ajout offres reservees + changement pending

--- app/controllers/grandma/offers_controller.rb
+++ app/controllers/grandma/offers_controller.rb
@@ -1,5 +1,6 @@
 class Grandma::OffersController < ApplicationController
   def index
     @bookings = Booking.joins(:offer).where(offers: { user_id: current_user.id })
+    @mybookings = Booking.where(offer_id: @offers)
   end
 end
