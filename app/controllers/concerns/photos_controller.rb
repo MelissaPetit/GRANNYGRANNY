3bea1d433c296790cfa28f4a3c4cfdb4566a6e2c
gabinrouyer <gabin.rouyer@gmail.com>
2024-03-15 12:06:18 +0100

    carousel

--- app/controllers/concerns/photos_controller.rb
+++
@@ -1,8 +0,0 @@
-class PhotosController < ApplicationController
-  def create
-    uploaded_file = params[:file]
-    cloudinary_response = Cloudinary::Uploader.upload(uploaded_file)
-    Photo.create(image_url: cloudinary_response['secure_url'])
-    redirect_to photos_path, notice: 'Photo uploaded successfully.'
-  end
-end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,7 +1,6 @@
 class Offer < ApplicationRecord
   CATEGORY = ["Sport", "Cuisine", "Tradition", "Activité manuelle", "Insolite"]
   has_many :bookings, dependent: :destroy
-  has_many :photos
   belongs_to :user
   validates :title, :start_from, :price_per_participant, :photo_url, :duration, :category, :address, :capacity, presence: true
   validates :description, length: { minimum: 10 }
