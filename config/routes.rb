a1fab7f41a3483ea30170769ea27dd32cc7c67be
Sylvain Bohrer <a3e7d6@gmail.com>
2024-03-15 03:07:23 +0100

    Ajout de la logique new pour créer une offre en tant que Granny - non terminé mais bien avancé - j'ai un pull propre

--- config/routes.rb
+++ config/routes.rb
@@ -18,14 +18,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  # resources :offers, only: [:index]
   get '/uikit', to: 'pages#uikit'
 
-  # user story d'une grand-mère:
   namespace :grandma do
     resources :bookings, only: [:index]
-    resources :offers, only: [:index]
+    resources :offers, only: [:index, :new, :create]
   end
 end
